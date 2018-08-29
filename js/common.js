e0dd62370b2915f94e09a02df7e551d1276a9175
AlexanderShaternik <alexandershaternik@gmail.com>
2018-08-30 01:05:34 +0300

    filter str length

--- js/common.js
+++ js/common.js
@@ -1,23 +1,29 @@
-//
 
-var xhr = new XMLHttpRequest();
-// xhr.withCredentials = true;
-xhr.addEventListener("readystatechange", function () {
-    if (this.readyState === 4) {
-        var result = JSON.parse(this.response);
-        console.log(result.data);
-    }
-});
-xhr.open("GET", "http://www.mrsoft.by/data.json",true);
-xhr.send();
-
-// fetch('http://www.mrsoft.by/data.json')
-//     .then(function(response) {
-//     return response.json();
-// }).then(function(result) {
-//     console.log(result.data);
-// }).catch(function(error) {
-//     console.log( error);
+// var xhr = new XMLHttpRequest();
+// xhr.addEventListener("readystatechange", function () {
+//     if (this.readyState === 4) {
+//         var res = JSON.parse(this.response);
+//         var result = res.data.filter((str)=> str.length > count);
+//         console.log(result)
+//     }
 // });
+// xhr.open("GET", "http://www.mrsoft.by/data.json",true);
+// xhr.send();
 
-//
+
+function out(){
+    fetch('http://www.mrsoft.by/data.json')
+        .then(function(response) {
+            return response.json();
+        }).then(function(res) {
+        var result = res.data.filter((str)=> str.length > document.querySelector('input[type=text]').value);
+        document.querySelector('.output p').innerHTML = result;
+    }).catch((error)=>console.log( error));
+}
+
+
+var first = document.querySelector('.first');
+first.addEventListener('click', out);
+
+var second = document.querySelector('.second');
+second.addEventListener('click', out);
