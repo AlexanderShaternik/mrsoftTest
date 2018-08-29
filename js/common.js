6705f82f3395d55f50c60fd03c95a0acc1b955b2
AlexanderShaternik <alexandershaternik@gmail.com>
2018-08-30 00:11:18 +0300

    small fix

--- js/common.js
+++ js/common.js
@@ -1,7 +1,7 @@
+//
 
 var xhr = new XMLHttpRequest();
 // xhr.withCredentials = true;
-
 xhr.addEventListener("readystatechange", function () {
     if (this.readyState === 4) {
         var result = JSON.parse(this.response);
@@ -9,15 +9,15 @@ xhr.addEventListener("readystatechange", function () {
     }
 });
 xhr.open("GET", "http://www.mrsoft.by/data.json",true);
-
 xhr.send();
 
+// fetch('http://www.mrsoft.by/data.json')
+//     .then(function(response) {
+//     return response.json();
+// }).then(function(result) {
+//     console.log(result.data);
+// }).catch(function(error) {
+//     console.log( error);
+// });
 
-fetch('http://www.mrsoft.by/data.json')
-    .then(function(response) {
-    return response.json();
-}).then(function(result) {
-    console.log(result.data);
-}).catch(function(error) {
-    console.log( error);
-});
+//
