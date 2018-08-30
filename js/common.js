0d1427940fe8caf5af703e5d51f0e81f101579ed
alexandershaternik <alexandershaternik@gmail.com>
2018-08-30 14:41:29 +0300

    filter by string

--- js/common.js
+++ js/common.js
@@ -9,21 +9,41 @@
 // });
 // xhr.open("GET", "http://www.mrsoft.by/data.json",true);
 // xhr.send();
-
-
-function out(){
-    fetch('http://www.mrsoft.by/data.json')
-        .then(function(response) {
-            return response.json();
-        }).then(function(res) {
+let promise = fetch('http://www.mrsoft.by/data.json')
+    .then(function(response) {
+        return response.json();
+    });
+function firstOut(){
+    promise
+        .then(function(res) {
         var result = res.data.filter((str)=> str.length > document.querySelector('input[type=text]').value);
         document.querySelector('.output p').innerHTML = result;
-    }).catch((error)=>console.log( error));
+        }).catch((error)=>document.querySelector('.output p').innerHTML = error);
+}
+
+
+function secondOut(){
+   if(document.querySelector('input[type=checkbox]').checked){
+       promise
+           .then(function(res) {
+               var result = res.data.filter(function(str){
+
+                   return str.toLowerCase().indexOf(document.querySelector('input[type=text]').value.toLowerCase()) > -1
+               });
+               document.querySelector('.output p').innerHTML = result.length+' совпадений '+'<br>'+result;;
+           }).catch((error)=>document.querySelector('.output p').innerHTML = error);
+   } else{
+       promise
+           .then(function(res) {
+               var result = res.data.filter((str)=> str.indexOf(document.querySelector('input[type=text]').value) > -1);
+               document.querySelector('.output p').innerHTML = result.length+' совпадений '+'<br>'+result;
+           }).catch((error)=>document.querySelector('.output p').innerHTML = error);
+   }
 }
 
 
 var first = document.querySelector('.first');
-first.addEventListener('click', out);
+first.addEventListener('click', firstOut);
 
 var second = document.querySelector('.second');
-second.addEventListener('click', out);
+second.addEventListener('click', secondOut);
