2394ef6dc21080c69e5a872e00b499a662940470
alexandershaternik <alexandershaternik@gmail.com>
2018-08-31 14:36:55 +0300

    comments

--- js/common.js
+++ js/common.js
@@ -1,39 +1,44 @@
-//
+//запрос по адресу http://www.mrsoft.by/data.json
 // var xhr = new XMLHttpRequest();
 // xhr.addEventListener("readystatechange", function () {
 //     if (this.readyState === 4) {
 //         var res = JSON.parse(this.response);
-//         var result = res.data.filter((str)=> str.length > count);
-//         console.log(result)
+//         console.log(res)
 //     }
 // });
 // xhr.open("GET", "http://www.mrsoft.by/data.json",true);
 // xhr.send();
+
+// fetch запрос по адресу http://www.mrsoft.by/data.json
+
 let promise = fetch('http://www.mrsoft.by/data.json')
     .then(function(response) {
         return response.json();
     });
+//фильтр по длине слов
 function firstOut(){
     promise
         .then(function(res) {
-        var result = res.data.filter((str)=> str.length > document.querySelector('input[type=text]').value);
+        let result = res.data.filter((str)=> str.length > document.querySelector('input[type=text]').value);
         document.querySelector('.output p').innerHTML = result.length+' совпадений '+'<br>'+result;
         }).catch((error)=>document.querySelector('.output p').innerHTML = error);
 }
 
-
+//фильтр по подстроке
 function secondOut(){
    if(document.querySelector('input[type=checkbox]').checked){
+       //checkbox чувствительности регистра проставлен
        promise
            .then(function(res) {
-               var result = res.data.filter((str)=> str.indexOf(document.querySelector('input[type=text]').value) > -1);
+               let result = res.data.filter((str)=> str.indexOf(document.querySelector('input[type=text]').value) > -1);
                document.querySelector('.output p').innerHTML = result.length+' совпадений '+'<br>'+result;
            }).catch((error)=>document.querySelector('.output p').innerHTML = error);
 
    } else{
+       // checkbox чувствительности регистра снят
        promise
            .then(function(res) {
-               var result = res.data.filter(function(str){
+               let result = res.data.filter(function(str){
 
                    return str.toLowerCase().indexOf(document.querySelector('input[type=text]').value.toLowerCase()) > -1
                });
@@ -42,9 +47,8 @@ function secondOut(){
    }
 }
 
-
-var first = document.querySelector('.first');
+let first = document.querySelector('.first');
 first.addEventListener('click', firstOut);
 
-var second = document.querySelector('.second');
+let second = document.querySelector('.second');
 second.addEventListener('click', secondOut);
